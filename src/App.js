1cd5a3efd67c487d2e39cb0d10751d6826bbe740
Manu9704 <manulovedad111@gmail.com>
2024-08-09 18:10:34 +0530

    mail message

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
+import { useEffect } from 'react';
 import Dashboard from './components/dashboard';
 import Sidebar from './components/sidebar';
 function App() {
+  useEffect(() => {
+    document.title = "Manohar Bandaru";
+  }, []);
   return (
     <div className="App">
       <Sidebar/>
