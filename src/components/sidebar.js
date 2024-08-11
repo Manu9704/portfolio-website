e2530ee0467aa115cd8491396e1b645431667b24
Manu9704 <manulovedad111@gmail.com>
2024-08-11 18:33:54 +0530

    responsive

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -10,6 +10,18 @@ import { IoLocationSharp } from "react-icons/io5";
 
 
 export default function Sidebar(){
+    const handleFaceBook = () => {
+        window.open('https://www.facebook.com/manohar.bandaru.35', '_blank', 'noopener,noreferrer')
+    }
+    const handleInstagram = () => {
+        window.open('https://www.instagram.com/the____manohar/', '_blank', 'noopener,noreferrer')
+    }
+    const handleTwitter = () => {
+        window.open('https://x.com/ManoharBandar16', '_blank', 'noopener,noreferrer')
+    }
+    const handleLinkedIn = () => {
+        window.open('https://linkedin.com/in/manohar-bandaru-2a7a881b3', '_blank', 'noopener,noreferrer')
+    }
     return(
         <div className="sidebar">
             <div className="top-sb">
@@ -23,7 +35,12 @@ export default function Sidebar(){
                 <div className="sb-det"><div className="sb-det-img"><FaMobileAlt/></div><div className="sb-det-text"><p>Phone</p><p>+91 9704621960</p></div></div>
                 <div className="sb-det"><div className="sb-det-img"><IoCalendarSharp/></div><div className="sb-det-text"><p>Date Of Birth</p><p>20 Feb 2000</p></div></div>
                 <div className="sb-det"><div className="sb-det-img"><IoLocationSharp/></div><div className="sb-det-text"><p>Location</p><p>Visakhapatnam, India</p></div></div>
-                <div className="sb-sm"><FaInstagram/><FaFacebookSquare/><FaLinkedin/><FaXTwitter/></div>
+                <div className="sb-sm">
+                   <div className="soc-card" onClick={handleFaceBook}><FaFacebookSquare/></div>
+                   <div className="soc-card" onClick={handleInstagram}><FaInstagram/></div>
+                   <div className="soc-card" onClick={handleTwitter}><FaXTwitter/></div>
+                   <div className="soc-card" onClick={handleLinkedIn}><FaLinkedin/></div>
+                </div>
             </div>
         </div>
     )
